90c68afdf5a487c76dca7b57556ecddba2c4ca56
Cristian Radulescu <cristian67@gmail.com>
2016-09-10 18:48:48 +0300

    Change config values for Heroku

--- web/index.php
+++ web/index.php
@@ -1,14 +1,31 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../config/default.php';
 
+use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
-$app = new Silex\Application();
+$app = new Application();
 $app['debug'] = true;
+
+// App Secret can be retrieved from the App Dashboard
+$app['app_secret'] = getenv('APP_SECRET') ?
+    getenv('APP_SECRET') : '';
+
+// Arbitrary value used to validate a webhook
+$app['page_access_token'] = getenv('PAGE_ACCESS_TOKEN') ?
+    getenv('PAGE_ACCESS_TOKEN') : '';
+
+// Generate a page access token for your page from the App Dashboard
+$app['validation_token'] = getenv('VALIDATION_TOKEN') ?
+    getenv('VALIDATION_TOKEN') : '';
+
+// URL where the app is running (include protocol). Used to point to scripts and
+// assets located at this address.
+$app['server_url'] = getenv('SERVER_URL') ?
+    getenv('SERVER_URL') : '';
 
 /**
  * @todo What to do here?
@@ -22,9 +39,9 @@ $app->get('/', function () {
  * setup is the same token used here.
  *
  */
-$app->get('/webhook/', function (Request $request) {
+$app->get('/webhook/', function (Application $app, Request $request) {
     if ('subscribe' === $request->get('hub_mode') &&
-        VALIDATION_TOKEN === $request->get('hub_verify_token')) {
+        $app['validation_token'] === $request->get('hub_verify_token')) {
             return new Response($request->get('hub_challenge'));
     }
 
