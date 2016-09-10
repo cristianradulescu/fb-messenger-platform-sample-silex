f3dee8a1f1b540392e471299751012514d6e4a14
Cristian Radulescu <cristian67@gmail.com>
2016-09-10 17:53:12 +0300

    Add description for config values

--- config/default.sample.php
+++ config/default.sample.php
@@ -1,6 +1,14 @@
 <?php
 
+// App Secret can be retrieved from the App Dashboard
 const APP_SECRET = '';
+
+// Arbitrary value used to validate a webhook
 const PAGE_ACCESS_TOKEN = '';
+
+// Generate a page access token for your page from the App Dashboard
 const VALIDATION_TOKEN = '';
+
+// URL where the app is running (include protocol). Used to point to scripts and
+// assets located at this address.
 const SERVER_URL = '';
