2d70057f69a43bfb3591fc6a7dda3b4969b2d767
Cristian Radulescu <cristian67@gmail.com>
2016-09-10 17:54:58 +0300

    Improved comments and to do; add more methods to sample app

--- web/index.php
+++ web/index.php
@@ -10,19 +10,38 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+/**
+ * @todo What to do here?
+ */
 $app->get('/', function () {
-    return 'URL list here';
+    return 'It works!?!';
 });
 
+/*
+ * Use your own validation token. Check that the token used in the Webhook
+ * setup is the same token used here.
+ *
+ */
 $app->get('/webhook/', function (Request $request) {
     if ('subscribe' === $request->get('hub_mode') &&
-      VALIDATION_TOKEN === $request->get('hub_verify_token')) {
-        return new Response($request->get('hub_challenge'));
+        VALIDATION_TOKEN === $request->get('hub_verify_token')) {
+            return new Response($request->get('hub_challenge'));
     }
 
     return new AccessDeniedHttpException(
         'Failed validation. Make sure the validation tokens match.'
     );
+});
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+$app->post('/webhook/', function (Request $request) {
+    return new Response('TO DO');
 });
 
 $app->run();
